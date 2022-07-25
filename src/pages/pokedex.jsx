dc068afbf4e457e3e1788bbee9bcd761ccf2cc38
Orion Antonucci <wagantonucci@hotmail.com>
2022-07-25 14:56:33 -0300

    create pokedex page and menu component

---
+++ src/pages/pokedex.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import MenuHamburguer from "../components/molecules/menuhamburguer/menuhamburguer"
+
+const Pokedex = () => {
+    return <MenuHamburguer></MenuHamburguer>
+}
+
+export default Pokedex;
