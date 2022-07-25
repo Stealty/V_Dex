e7d44c2aaf39dc3f166a2e665905342209f7d565
Gabriel Ribeiro <gabriel.ribeiro@valtech.com>
2022-07-25 13:47:31 -0300

    Alias paths for imports

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@templates": path.resolve(__dirname, "src/templates"),
+      "@atoms": path.resolve(__dirname, "src/components/atoms"),
+      "@molecules": path.resolve(__dirname, "src/components/molecules"),
+      "@organisms": path.resolve(__dirname, "src/components/organisms"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@api": path.resolve(__dirname, "src/api"),
+    },
+  },
+});
