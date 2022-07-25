cf2c75afa23766efbbed5294981547dfdc2fffb3
Gabriel Ribeiro <gabriel.ribeiro@valtech.com>
2022-07-25 15:01:47 -0300

    home background and title position

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./pages/home";
+import "./styles/global.css";
+import { HomeScreen } from "@/templates";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <HomeScreen />
   </React.StrictMode>
 );
