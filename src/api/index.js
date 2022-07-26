8dc4c71e8107a212705bffbf5da6507d83dce6e0
Gabriel Ribeiro <gabriel.ribeiro@valtech.com>
2022-07-26 11:17:25 -0300

    feature/RTK query api

---
+++ src/api/index.js
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+export const pokemonApi = createApi({
+  reducerPath: "pokemonApi",
+  baseQuery: fetchBaseQuery({ baseUrl: `https://pokeapi.co/api/v2/pokemon/` }),
+  keepUnusedDataFor: 86400,
+  endpoints: (builder) => ({
+    getPokemon: builder.query({
+      query: (limit) => `?limit=${limit}`,
+    }),
+  }),
+});
+
+export const pokemonDetailsApi = createApi({
+  reducerPath: "pokemonDetailsApi",
+  baseQuery: fetchBaseQuery({ baseUrl: undefined }),
+  keepUnusedDataFor: 86400,
+  endpoints: (builder) => ({
+    getPokemonDetails: builder.query({
+      query: (url) => url,
+    }),
+  }),
+});
+
+export const { useGetPokemonDetailsQuery } = pokemonDetailsApi;
+
+export const { useGetPokemonQuery } = pokemonApi;
--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,8 @@ export default defineConfig({
       "@molecules": path.resolve(__dirname, "src/components/molecules"),
       "@organisms": path.resolve(__dirname, "src/components/organisms"),
       "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@store": path.resolve(__dirname, "src/store"),
+      "@api": path.resolve(__dirname, "src/api"),
     },
   },
 });
