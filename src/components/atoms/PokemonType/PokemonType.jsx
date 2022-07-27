318a4e80b0898ecba8c31420aff46723ad659534
Gabriel Ribeiro <gabriel.ribeiro@valtech.com>
2022-07-27 10:08:49 -0300

    Title component name changed to Paragraph

--- src/components/atoms/PokemonType/PokemonType.jsx
+++ src/components/atoms/PokemonType/PokemonType.jsx
@@ -2,5 +2,5 @@ import styles from "./PokemonType.module.scss";
 import firstUppercase from "../../../utils/firstUppercase";
 
 export default function PokemonType({ type }) {
-  return <span className={styles.PokemonType}>{firstUppercase(type)}</span>;
+  return <span className={styles.PokemonType}>{type}</span>;
 }
