d15eea1a19448a45b424992c927070463372e601
Orion Antonucci <wagantonucci@hotmail.com>
2022-07-27 13:23:19 -0300

    create modal and filter

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,3 +1,6 @@
-export { default as PokemonCard } from "./PokemonCard/PokemonCard";
 export { default as CategoriesCard } from "./CategoriesCard/CategoriesCard";
+export { default as Filter } from "./Filter/Filter";
 export { default as MenuHamburguer } from "./menuhamburguer/menuhamburguer";
+export { default as Modal } from "./Modal/Modal";
+export { default as ModalObject } from "./ModalObject/ModalObject";
+export { default as PokemonCard } from "./PokemonCard/PokemonCard";
--- src/templates/PokedexScreen/PokedexScreen.jsx
+++ src/templates/PokedexScreen/PokedexScreen.jsx
@@ -1,16 +1,24 @@
-import { PokemonCard } from "@molecules";
+import { PokemonCard, Modal, Filter } from "@molecules";
 import { Title } from "@atoms";
 import styles from "./PokedexScreen.module.scss";
 import { useGetPokemonQuery } from "@api";
+import { useState } from "react";
 
 const PokedexScreen = () => {
     const { data, isLoading, error } = useGetPokemonQuery(30);
+    const [isActive, setActive] = useState(false);
+
+    const onClickHandler = () => {
+        setActive(!isActive);
+    }
 
     if (!isLoading) {
         return <div className={styles.PokedexScreen}>
             <div className={styles.PokemonList}>
                 {data.results.map((pokemon) => <PokemonCard key={pokemon.name} pokemon={pokemon}></PokemonCard>)}
             </div>
+            <Modal Active={isActive}></Modal>
+            <Filter onClick={onClickHandler} Active={isActive}></Filter>
         </div>
     } else {
         return <Title title="No Pokémon Found"></Title>
