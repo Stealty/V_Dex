b168361cf06d5886a260f4f68fd25ad64561f7e2
Gabriel Ribeiro <gabriel.ribeiro@valtech.com>
2022-07-29 09:01:13 -0300

    fetch and store pokemon species

--- src/api/index.js
+++ src/api/index.js
@@ -23,6 +23,21 @@ export const pokemonDetailsApi = createApi({
   }),
 });
 
+export const pokemonSpeciesApi = createApi({
+  reducerPath: "pokemonSpeciesApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `https://pokeapi.co/api/v2/pokemon-species/`,
+  }),
+  keepUnusedDataFor: 86400,
+  endpoints: (builder) => ({
+    getPokemonSpecies: builder.query({
+      query: (pokemonName) => pokemonName,
+    }),
+  }),
+});
+
 export const { useGetPokemonDetailsQuery } = pokemonDetailsApi;
 
 export const { useGetPokemonQuery } = pokemonApi;
+
+export const { useGetPokemonSpeciesQuery } = pokemonSpeciesApi;
--- src/store/modules/configureStore.js
+++ src/store/modules/configureStore.js
@@ -2,18 +2,21 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { pokemonApi, pokemonDetailsApi } from "@api";
 import pokemonSliceReducer from "@store/modules/pokemonSlice";
+import { pokemonSpeciesApi } from "../../api";
 
 export const store = configureStore({
   reducer: {
     [pokemonApi.reducerPath]: pokemonApi.reducer,
     [pokemonDetailsApi.reducerPath]: pokemonDetailsApi.reducer,
+    [pokemonSpeciesApi.reducerPath]: pokemonSpeciesApi.reducer,
     pokemon: pokemonSliceReducer,
   },
 
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
       pokemonApi.middleware,
-      pokemonDetailsApi.middleware
+      pokemonDetailsApi.middleware,
+      pokemonSpeciesApi.middleware
     ),
 });
 
