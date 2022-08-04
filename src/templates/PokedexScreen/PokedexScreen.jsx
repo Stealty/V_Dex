2f76d73edf5f3df16e5534b2f9e7289d246123cb
Gabriel Ribeiro <gabriel.ribeiro@valtech.com>
2022-08-04 08:59:28 -0300

    fix/Pokedex with two datas. One has been removed

--- src/templates/PokedexScreen/PokedexScreen.jsx
+++ src/templates/PokedexScreen/PokedexScreen.jsx
@@ -1,14 +1,10 @@
 import { PokemonCard, Modal, Filter, MenuHamburguer } from "@molecules";
 import { Backdrop, SearchBar } from "@atoms";
 import styles from "./PokedexScreen.module.scss";
-import { useSelector } from "react-redux";
 import { useEffect, useRef, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 const PokedexScreen = ({ data }) => {
-  const AllPokemon = useSelector((state) =>
-    state.pokemon.pokemonSpecies.slice()
-  );
   const [isLoading, setLoading] = useState(false);
   const [pokemon, SetPokemons] = useState(data.slice(0, 40));
   const [isActive, setActive] = useState(false);
@@ -19,7 +15,7 @@ const PokedexScreen = ({ data }) => {
   useEffect(() => {
     if (state) {
       SetPokemons(
-        AllPokemon.filter((pokemon) =>
+        data.filter((pokemon) =>
           pokemon.name.toString().toLowerCase().includes(state)
         )
       );
@@ -38,7 +34,7 @@ const PokedexScreen = ({ data }) => {
     setLoading(true);
     setInterval(() => {
       SetPokemons(
-        AllPokemon.filter((pokemon) =>
+        data.filter((pokemon) =>
           pokemon.name
             .toString()
             .toLowerCase()
